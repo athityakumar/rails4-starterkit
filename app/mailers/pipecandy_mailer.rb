a8229fac319a18cc5527c7b61a1a12080fd60080
thangaduraicse <thangadurai@contractiq.com>
2016-05-27 11:50:43 +0530

    Update email ID to thangadurai@contractiq.in for error mail handler

--- app/mailers/pipecandy_mailer.rb
+++ app/mailers/pipecandy_mailer.rb
@@ -15,39 +15,39 @@ class PipecandyMailer < ApplicationMailer
 
   def twitter_autofollow_error error_subject, error_message
     @error_message = error_message
-    mail(to: "rajesh@pipecandy.com", subject: error_subject)
+    mail(to: "thangadurai@contractiq.in", subject: error_subject)
   end
 
   def twitter_update_followback_status_mailer followers, new_follower_ids
     @followers = followers
     @new_follower_ids = new_follower_ids
-    mail(to: "rajesh@pipecandy.com, ashwin@pipecandy.com", subject: "Twitter Follow Update - New Follow Back Friends For PipeCandy! @ #{Time.now}")
+    mail(to: "thangadurai@contractiq.in, ashwin@pipecandy.com", subject: "Twitter Follow Update - New Follow Back Friends For PipeCandy! @ #{Time.now}")
   end
 
   def twitter_rake_success status, twitter_ids=[]
     @status = status
     @twitter_ids = twitter_ids
-    mail(to: "rajesh@pipecandy.com", subject: "Twitter #{@status.to_s.capitalize} Success at #{Time.now}")
+    mail(to: "thangadurai@contractiq.in", subject: "Twitter #{@status.to_s.capitalize} Success at #{Time.now}")
   end
 
   def twitter_fetch_create_follower_error error_message
     @error_message = error_message
-    mail(to: "rajesh@pipecandy.com", subject: "Twitter - Fetch & Create Followers Error at #{Time.now}")
+    mail(to: "thangadurai@contractiq.in", subject: "Twitter - Fetch & Create Followers Error at #{Time.now}")
   end
 
   def twitter_fetch_create_follower name
     @username = name
-    mail(to: "rajesh@pipecandy.com", subject: "Twitter - Fetch & Create Followers Added at #{Time.now}")
+    mail(to: "thangadurai@contractiq.in", subject: "Twitter - Fetch & Create Followers Added at #{Time.now}")
   end
 
   def inbound_fetch_create_error error_message
     @error_message = error_message
-    mail(to: "athityakumar@gmail.com", subject: "Inbound - Fetch & Create Users Error at #{Time.now}")
+    mail(to: "thangadurai@contractiq.in", bcc: "athityakumar@gmail.com", subject: "Inbound - Fetch & Create Users Error at #{Time.now}")
   end
 
   def inbound_autofollow_error error_subject, error_message
     @error_message = error_message
-    mail(to: "athityakumar@gmail.com", subject: error_subject)
+    mail(to: "thangadurai@contractiq.in", bcc: "athityakumar@gmail.com", subject: error_subject)
   end
 
 end
