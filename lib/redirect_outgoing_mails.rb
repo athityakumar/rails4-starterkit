25a5990c821679569cb780ea893804eaab180381
sathishkumar <sathish@contractiq.in>
2016-06-22 13:57:01 +0530

    moved sathish@i2i mails to sathish@ctiq

--- lib/redirect_outgoing_mails.rb
+++ lib/redirect_outgoing_mails.rb
@@ -1,9 +1,9 @@
 class RedirectOutgoingMails
   class << self
     def delivering_email(mail)
-      mail.to = 'sathishkumar@ideas2it.com'
-      mail.cc =  'sathishkumar@ideas2it.com' if !mail.cc.nil?
-      mail.bcc = 'sathishkumar@ideas2it.com' if !mail.bcc.nil?
+      mail.to = 'sathish@contractiq.in'
+      mail.cc =  'sathish@contractiq.in' if !mail.cc.nil?
+      mail.bcc = 'sathish@contractiq.in' if !mail.bcc.nil?
     end
   end
 end
